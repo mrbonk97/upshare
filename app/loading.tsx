45bee97f027ba46dba9d7a2f9da3f0fb972f6814
mrbonk97 <mrbonk97@gmail.com>
2024-05-28 15:45:00 +0900

    refactor: double quote 변경

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,10 @@
+import { Spinner } from "@/components/spinner";
+
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <main className='h-full w-full flex2'>
-      <article></article>
+    <main className="h-full w-full flex2">
+      <Spinner loading={true} />
     </main>
   );
 }
--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -1,8 +1,10 @@
+"use client";
+import { Spinner } from "@/components/spinner";
+
 export default function LoadingPage() {
-  // You can add any UI inside Loading, including a Skeleton.
   return (
-    <main className='h-full w-full flex2'>
-      <h2 className='text-secondary text-4xl font-semibold'>로딩중...</h2>
+    <main className="h-full w-full flex2">
+      <Spinner loading />
     </main>
   );
 }
