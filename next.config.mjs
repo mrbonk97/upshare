feeb783847490b286f89bb7458921f79b9192520
mrbonk97 <mrbonk97@gmail.com>
2024-05-29 15:04:58 +0900

    feat: 깃허브 배포 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    loader: "custom",
+    loaderFile: "/utils/image-loader.ts",
+  },
+};
 
 export default nextConfig;
---
+++ utils/image-loader.ts
@@ -0,0 +1,5 @@
+const ImageLoader = ({ src }: { src: string }) => {
+  return `https://mrbonk97.github.io/file-client/images/${src}`;
+};
+
+export default ImageLoader;
