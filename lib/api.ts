b2f0dddca03a6836d584868e523087ffcf9309f9
mrbonk97 <mrbonk97@gmail.com>
2024-06-26 13:41:16 +0900

    refresh token 설정

--- lib/api.ts
+++ lib/api.ts
@@ -11,3 +11,24 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (req) => req,
+  async (error) => {
+    const res = await axios.get(
+      process.env.NEXT_PUBLIC_API_URL + "/tokens/refresh",
+      {
+        withCredentials: true,
+        headers: {
+          "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_URL,
+        },
+      }
+    );
+
+    if (res.status == 200) {
+      error.config.headers["Authorization"] = `Bearer ${res.data.access_token}`;
+      localStorage.setItem("access_token", res.data.access_token);
+      return axios(error.config);
+    }
+  }
+);
